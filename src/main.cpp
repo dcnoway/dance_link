b988fc2be02a17b6149273d2803423808da5f8c5
Wills Wu <willswu@outlook.com>
2020-07-18 06:53:08 +0800

    finetune CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char const *argv[])
 {
     LinkNode<int> root(0);
     LinkNode<int> temp(-1);
-    // root.insertLeft(std::move(temp));
-    // root.insertRight(LinkNode<int>(1)).insertRight(LinkNode<int>(2));
+    // root.insertLeft(std::forward(temp));
+    // root.insertRight(std::move(LinkNode<int>(1)));
+    // .insertRight(LinkNode<int>(2));
     return 0;
 }
