5154a132d176db171bd3b84a86bc9404bbe125f3
Wills Wu <willswu@outlook.com>
2020-07-23 20:40:20 +0800

    scratch 2

--- include/dancing_link.hpp
+++ include/dancing_link.hpp
@@ -2,48 +2,60 @@
 #include <memory>
 #include <stack>
 
-template<typename T>
-class _LinkNode:public std::enable_shared_from_this<_LinkNode<T>>
+namespace DancingLink
 {
+    template <typename T>
+    class _LinkNode //: public std::enable_shared_from_this<_LinkNode<T>>
+    {
     public:
-    T val;
-    std::shared_ptr<_LinkNode<T>> sp_right;
-    std::shared_ptr<_LinkNode<T>> sp_down;
-    std::weak_ptr<_LinkNode<T>> sp_up;
-    std::weak_ptr<_LinkNode<T>> sp_left;
-    // Do NOT disable copy, we need push obj to stack
-    // Disable temprorily to check rvalue functions is working or not.
-    _LinkNode(const _LinkNode<T> & a) noexcept;//=delete;
-    _LinkNode<T>& operator=(const _LinkNode<T> & a) noexcept;//=delete;
+        T val;
+        std::shared_ptr<_LinkNode<T>> sp_right;
+        std::shared_ptr<_LinkNode<T>> sp_down;
+        std::weak_ptr<_LinkNode<T>> sp_up;
+        std::weak_ptr<_LinkNode<T>> sp_left;
+        // Do NOT disable copy, we need push obj to stack
+        // Disable temprorily to check rvalue functions is working or not.
+        // _LinkNode(const _LinkNode<T> & a) noexcept;//=delete;
+        // _LinkNode<T>& operator=(const _LinkNode<T> & a) noexcept;//=delete;
 
-    _LinkNode():val(){};
-    _LinkNode(const T && v):val(v){};
-    _LinkNode(_LinkNode<T> && rval):sp_right(rval.sp_right),sp_left(rval.sp_left),sp_up(rval.sp_up),sp_down(rval.sp_down){
-        rval.sp_right = rval.sp_down = nullptr;
-        rval.sp_left.reset();
-        rval.sp_up.reset();
-    }
+        _LinkNode() : val(){};
+        _LinkNode(const T &&v) : val(v){};
+        _LinkNode(_LinkNode<T> &&rval) : sp_right(rval.sp_right), sp_left(rval.sp_left), sp_up(rval.sp_up), sp_down(rval.sp_down)
+        {
+            rval.sp_right = rval.sp_down = nullptr;
+            rval.sp_left.reset();
+            rval.sp_up.reset();
+        }
 
-    using std::enable_shared_from_this<_LinkNode<T>>::shared_from_this;
-};
+        //using std::enable_shared_from_this<_LinkNode<T>>::shared_from_this;
+    };
 
+    template<typename T>
+    using pnode_t = typename std::shared_ptr<_LinkNode<T>>;
 
-template<typename T>
-class DancingLink
-{
-    using node_t = _LinkNode<T>;
+    template <typename T>
+    class DancingLink
+    {
     private:
-    node_t _origin;
-    std::shared_ptr<node_t> sp_origin, sp_curr;
-    std::stack<node_t> _state_stack;
+        // node_t _origin;
+        std::shared_ptr<_LinkNode<T>> sp_origin, sp_curr;
+        std::stack<std::shared_ptr<_LinkNode<T>>> _state_stack;
+
     protected:
+        //template<typename T>
+        //using pnode = typename std::shared_ptr<_LinkNode<T>>;
+        const pnode_t<T> clone_all()  noexcept;
+        // const std::shared_ptr<_LinkNode<T>> clone_all() const noexcept;
+
     public:
-    DancingLink():_state_stack(){sp_origin=std::make_shared(_origin);sp_curr=sp_origin;}
+        DancingLink() : _state_stack() {std::cout<<"DancingLink constractor"<<std::endl;}
 
-    //Disable copy
-    DancingLink(const DancingLink &)=delete;
-    DancingLink & operator = (const DancingLink &)=delete;
+        //Disable copy
+        DancingLink(const DancingLink &) = delete;
+        DancingLink &operator=(const DancingLink &) = delete;
 
-    void push_state();//Incorrect, need impl
-    bool pop_state();
-};
+        void push_state();
+        bool pop_state();
+    };
+
+} // namespace DancingLink
--- src/dancing_link.cpp
+++ src/dancing_link.cpp
@@ -1,27 +1,27 @@
 #include "../include/dancing_link.hpp"
 
-template<typename T>
-_LinkNode<T>::_LinkNode(const _LinkNode<T> & v) noexcept
-{
-    std::cout<<" Copy constraction function called!" <<std::endl;
-    val = v.val;
-    sp_left = v.sp_left;
-    sp_right = v.sp_right;
-    sp_up = v.sp_up;
-    sp_down = v.sp_down;
-}
+// template<typename T>
+// _LinkNode<T>::_LinkNode(const _LinkNode<T> & v) noexcept
+// {
+//     std::cout<<" Copy constraction function called!" <<std::endl;
+//     val = v.val;
+//     sp_left = v.sp_left;
+//     sp_right = v.sp_right;
+//     sp_up = v.sp_up;
+//     sp_down = v.sp_down;
+// }
 
-template<typename T>
-_LinkNode<T> & _LinkNode<T>::operator=(const _LinkNode<T>& v) noexcept
-{
-    std::cout<<"Copy operator = function called!" <<std::endl;
-    val = v.val;
-    sp_left = v.sp_left;
-    sp_right = v.sp_right;
-    sp_up = v.sp_up;
-    sp_down = v.sp_down;
-    return *this;
-}
+// template<typename T>
+// _LinkNode<T> & _LinkNode<T>::operator=(const _LinkNode<T>& v) noexcept
+// {
+//     std::cout<<"Copy operator = function called!" <<std::endl;
+//     val = v.val;
+//     sp_left = v.sp_left;
+//     sp_right = v.sp_right;
+//     sp_up = v.sp_up;
+//     sp_down = v.sp_down;
+//     return *this;
+// }
 
 //     //insert _LinkNode to given direction, return a reference to the inserted node
 // template<typename T>
@@ -79,17 +79,38 @@ _LinkNode<T> & _LinkNode<T>::operator=(const _LinkNode<T>& v) noexcept
 //     return std::move(val);
 // }
 
-template<typename T>
-void DancingLink<T>::push_state() 
-{ 
-    _state_stack.push(*sp_origin); 
-} //Incorrect, need impl
-
-template<typename T>
-bool DancingLink<T>::pop_state()
+namespace DancingLink
 {
-    if(_state_stack.emplace())return false;
-    sp_origin = _state_stack.top();
-    _state_stack.pop();
-    return true;
-} //Incorrect, need impl
+
+    template <typename T>
+    void DancingLink<T>::push_state()
+    {
+        _state_stack.push(*sp_origin);
+    } //Incorrect, need impl
+
+    template <typename T>
+    bool DancingLink<T>::pop_state()
+    {
+        if (_state_stack.emplace())
+            return false;
+        sp_origin = _state_stack.top();
+        _state_stack.pop();
+        return true;
+    } //Incorrect, need impl
+
+    //        const std::shared_ptr<_LinkNode<T>> clone_all() const noexcept;
+    // template <typename T>
+    // const std::shared_ptr<_LinkNode<T>> DancingLink<T>::clone_all() const noexcept
+    // {
+    // }
+
+    // template <typename T>
+    // using pnode = typename std::shared_ptr<_LinkNode<T>>;
+
+    template <typename T>
+    const pnode_t<T> DancingLink<T>::clone_all()  noexcept
+    {
+        return nullptr;
+    }
+
+} // namespace DancingLink
