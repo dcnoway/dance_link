f630fa2de35e9a014d54d31d3401d3b232ce973d
Wills Wu <willswu@outlook.com>
2020-07-25 01:43:07 +0800

    rough test

--- src/main.cpp
+++ src/main.cpp
@@ -12,5 +12,31 @@ int main(int argc, char const *argv[])
     std::cout << std::boolalpha<< link.empty() <<std::endl;
     link.delete_all();
     std::cout << std::boolalpha<< link.empty() <<std::endl;
+
+    link.init_cols(10);
+    auto p1 = link.insert(2,2,1);
+    std::cout<<*p1<<std::endl;
+
+    link.insert(3,1,31);
+    link.insert(3,2,32);
+    auto p2 = link.insert(3,3,33);
+    if(p2){
+        std::cout<<"After insert"<<std::endl<<"Node:";
+        std::cout<<*p2;
+        std::cout<<"Column header:";
+        std::cout<<*(p2->wp_up.lock());
+
+        p2 = link.mark_remove(p2);
+        std::cout<<"After mark_remove"<<std::endl<<"Node:";
+        std::cout<<*p2;
+        std::cout<<"Column header:";
+        std::cout<<*(p2->wp_up.lock());
+
+        p2 = link.mark_restore(p2);
+        std::cout<<"After mark_restore"<<std::endl<<"Node:";
+        std::cout<<*p2;
+        std::cout<<"Column header:";
+        std::cout<<*(p2->wp_up.lock());
+    }
     return 0;
 }
