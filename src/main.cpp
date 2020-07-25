b7f1fb36c00dba7236fc3d5e1deacb3c1276eaae
Will Wu <willswu@outlook.com>
2020-07-25 20:34:53 +0800

    simple check dlx

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,32 @@
 
 int main(int argc, char const *argv[])
 {
-    std::string str =R"(C:\Windows)";
-    std::cout <<"windows path is "<<str <<std::endl;
-    std::cout<<"C++ version: "<<__cplusplus<<std::endl;
-    std::cout << "main()" << std::endl;
+    // std::string str =R"(C:\Windows)";
+    // std::cout <<"Check raw string: "<<str <<std::endl;
+    // std::cout<<"Check C++ standard version: "<<__cplusplus<<std::endl;
+    // std::cout << "Word Length:" <<sizeof(&argc)*8<<" bit"<< std::endl;
     dlx::link<int> link;
     link.init_cols(10);
-    std::cout << std::boolalpha<< link.empty() <<std::endl;
-    link.delete_all();
-    std::cout << std::boolalpha<< link.empty() <<std::endl;
 
-    link.init_cols(10);
     auto p1 = link.insert(2,2,1);
-    std::cout<<*p1<<std::endl;
-
     link.insert(3,1,31);
     link.insert(3,2,32);
     auto p2 = link.insert(3,3,33);
-    if(p2){
-        std::cout<<"After insert"<<std::endl<<"Node:";
-        std::cout<<*p2;
-        std::cout<<"Column header:";
-        std::cout<<*(p2->wp_up.lock());
 
-        p2 = link.mark_remove(p2);
-        std::cout<<"After mark_remove"<<std::endl<<"Node:";
-        std::cout<<*p2;
-        std::cout<<"Column header:";
-        std::cout<<*(p2->wp_up.lock());
+    std::cout <<"Before remove col header:"<<std::endl;
+    std::cout <<"Col header addr:" << p1->wp_col.lock() <<std::endl;
+    std::cout <<"Right of col is " <<*(p1->wp_col.lock()->sp_right)<<std::endl;
+    std::cout <<"Left of col is " << *(p1->wp_col.lock()->wp_left.lock())<<std::endl;
+    auto col = link.remove_col(p1->wp_col.lock());
+    if(col){
+        std::cout <<"After remove col header:" <<std::endl;
+        std::cout <<"Right of col is " <<*(p1->wp_col.lock()->sp_right)<<std::endl;
+        std::cout <<"Left of col is " <<*(p1->wp_col.lock()->wp_left.lock())<<std::endl;
 
-        p2 = link.mark_restore(p2);
-        std::cout<<"After mark_restore"<<std::endl<<"Node:";
-        std::cout<<*p2;
-        std::cout<<"Column header:";
-        std::cout<<*(p2->wp_up.lock());
+        link.restore_col(col);
+        std::cout <<"After restore col header:" <<std::endl;
+        std::cout <<"Right of col is " <<*(p1->wp_col.lock()->sp_right)<<std::endl;
+        std::cout <<"Left of col is " <<*(p1->wp_col.lock()->wp_left.lock())<<std::endl;
     }
     return 0;
 }
